934007815244bb486be93680c181fa3cef62ff37
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-09-25 11:32:54 +0200

    Initial commit

---
+++ NextBuild2018/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NextBuild2018
+//
+//  Created by Stefan van den Oord on 25/09/2018.
+//  Copyright © 2018 Stefan van den Oord. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
