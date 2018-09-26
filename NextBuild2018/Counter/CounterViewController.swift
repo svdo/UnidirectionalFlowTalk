099593b475809fb887a2b35857f4b650e30c4790
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-09-26 21:56:09 +0200

    Binds counter label

--- NextBuild2018/Counter/CounterViewController.swift
+++ NextBuild2018/Counter/CounterViewController.swift
@@ -3,11 +3,18 @@ import ReSwift
 import ReRxSwift
 
 class CounterViewController: UIViewController {
+    @IBOutlet weak var counterLabel: UILabel!
+    
     let connection = Connection(
         store: appStore,
         mapStateToProps: mapStateToProps,
         mapDispatchToActions: mapDispatchToActions
     )
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        connection.bind(\Props.counterValue, to: counterLabel.rx.text) { String($0) }
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -21,12 +28,16 @@ class CounterViewController: UIViewController {
 }
 
 extension CounterViewController: Connectable {
-    struct Props {}
+    struct Props {
+        let counterValue: Int
+    }
     struct Actions {}
 }
 
 private let mapStateToProps = { (appState: AppState) in
-    return CounterViewController.Props()
+    return CounterViewController.Props(
+        counterValue: appState.counter.value
+    )
 }
 
 private let mapDispatchToActions = { (dispatch: @escaping DispatchFunction) in
