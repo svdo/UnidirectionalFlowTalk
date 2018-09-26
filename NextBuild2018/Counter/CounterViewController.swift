364f7285b118c2c833b6b6d8e9d91cd6af31e85a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-09-26 21:45:51 +0200

    Makes CounterViewController a 'smart' component

--- NextBuild2018/Counter/CounterViewController.swift
+++ NextBuild2018/Counter/CounterViewController.swift
@@ -1,4 +1,35 @@
 import UIKit
+import ReSwift
+import ReRxSwift
 
 class CounterViewController: UIViewController {
+    let connection = Connection(
+        store: appStore,
+        mapStateToProps: mapStateToProps,
+        mapDispatchToActions: mapDispatchToActions
+    )
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        connection.connect()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        connection.disconnect()
+    }
 }
+
+extension CounterViewController: Connectable {
+    struct Props {}
+    struct Actions {}
+}
+
+private let mapStateToProps = { (appState: AppState) in
+    return CounterViewController.Props()
+}
+
+private let mapDispatchToActions = { (dispatch: @escaping DispatchFunction) in
+    return CounterViewController.Actions()
+}
+
