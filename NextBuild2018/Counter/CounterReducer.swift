fc5db40767c6ab0f3d10c7818e4df0c06ca09332
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-09-27 08:45:22 +0200

    Defines action and reducer logic for incrementing counter state

--- NextBuild2018/Counter/CounterReducer.swift
+++ NextBuild2018/Counter/CounterReducer.swift
@@ -3,5 +3,12 @@ import ReSwift
 private let initialState = CounterState(value: 0)
 
 func counterReducer(action: Action, state: CounterState?) -> CounterState {
-    return state ?? initialState
+    var state = state ?? initialState
+    switch action {
+    case is IncrementCounter:
+        state.value += 1
+    default:
+        break
+    }
+    return state
 }
