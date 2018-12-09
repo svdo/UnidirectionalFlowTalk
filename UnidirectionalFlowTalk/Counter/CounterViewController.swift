b4d1113c809a0ec937e3dcb8823b4de8ed412650
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-12-09 21:31:29 +0100

    Makes CounterViewController a 'smart' component

--- UnidirectionalFlowTalk/Counter/CounterViewController.swift
+++ UnidirectionalFlowTalk/Counter/CounterViewController.swift
@@ -1,8 +1,40 @@
 import UIKit
+import ReSwift
+import ReRxSwift
 
 class CounterViewController: UIViewController {
     @IBOutlet weak var counterLabel: UILabel!
-    
+
+    let connection = Connection(
+        store: appStore,
+        mapStateToProps: mapStateToProps,
+        mapDispatchToActions: mapDispatchToActions
+    )
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        connection.connect()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        connection.disconnect()
+    }
+
     @IBAction func incrementTapped() {
     }
 }
+
+extension CounterViewController: Connectable {
+    struct Props {}
+    struct Actions {}
+}
+
+private let mapStateToProps = { (appState: AppState) in
+    return CounterViewController.Props()
+}
+
+private let mapDispatchToActions = { (dispatch: @escaping DispatchFunction) in
+    return CounterViewController.Actions()
+}
+
